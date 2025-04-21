eee265c188360f3ee1acccdf8791da352f53e403
Julián Ramírez <judramirezva@gmail.com>
2025-04-21 18:08:25 -0500

    feat: add build configuration for chunking and size warning limit in Vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Split vendor libraries
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit if needed
+  },
 });
