9c841e64c376162cd64df391026037e40ca88e6a
Julián Ramírez <judramirezva@gmail.com>
2025-04-21 18:00:53 -0500

    style: update header styles for Login and Register pages

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -42,9 +42,9 @@ export const RegisterPage = () => {
                   d='M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5'
                 />
               </svg>
-              <h1 className='text-2xl font-semibold'>CalendarApp</h1>
+              <h1 className='text-3xl font-bold'>CalendarApp</h1>
             </div>
-            <h2 className='mt-10 text-center text-2xl/9 font-bold tracking-tight text-white'>
+            <h2 className='mt-10 text-center text-2xl'>
               Crea una nueva cuenta
             </h2>
 
